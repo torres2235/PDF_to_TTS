1688e7b6b77fc6b7a08f7acc8ad75cddd7a314ba
Torres2235 <Torres2235@gmail.com>
2024-08-08 16:28:35 -0700

    changed API key into an environment variable

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from pypdf import PdfReader
+import os
 
 
-API_KEY = 'BDE8iVtvKyo3sw5eh-pw6nsqdfkiYpPdsNhWX_O1ih8Y'
+API_KEY = os.environ.get("IBM_API_KEY")
 ENDPOINT = 'https://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/cbdc45d4-a40d-4764-997f-6a4d0937beb6'
-
+print(os.getenv("IBM_API_KEY"))
 
 authenticator = IAMAuthenticator(API_KEY)
 text_to_speech = TextToSpeechV1(
@@ -24,7 +25,7 @@ for page in reader.pages:
 tts_name = input("Name your TTS file (do not include file type): ")
 voice_to_use = 'en-US_AllisonV3Voice'
 
-print("Generating TTS file, please wait, this may take a minute . . .")
+print("Generating TTS file, please wait, this may take a few minutes . . .")
 with open(f'converted/{tts_name}.mp3', 'wb+') as audio_file:
     audio_file.write(
         text_to_speech.synthesize(
